09140f678c9a026eb996d9aa2f7972a237ffa5e8
Elli Lehtimäki <elli.i.lehtimaki@gmail.com>
2023-05-03 11:55:53 +0300

    Commented

--- main.cpp
+++ main.cpp
@@ -1,6 +1,23 @@
+/* Errors
+ *
+ * Description
+ *
+ * Program asks the user to give a word to be guessed. Program "cleans" the
+ * screen by printing empty lines that the word won't show. Program checks after
+ * every guessed letter if the user has already guessed the word and prints the
+ * gamestatus from the information gotten. It also counts the amount of times
+ * user has guessed and if user uses all the guesses, game ends by printing
+ * "Guesses expired!".
+ *
+ * Writer of the program
+ *
+ * Name: EILeh
+ */
+
+
+#include <cctype>
 #include <iostream>
 #include <string>
-#include <cctype>
 
 using namespace std;
 
